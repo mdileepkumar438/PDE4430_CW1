ed8806cd5b9a42f2b62e78e7a323f1ea106e9587
Dileep <dileep@gmail.com>
2022-11-13 12:02:11 +0400

    Comments has been added to the program for Clarification

--- scripts/turtle_teleoperation.py
+++ scripts/turtle_teleoperation.py
@@ -4,6 +4,8 @@ import getch
 import rospy
 from geometry_msgs.msg import Twist
 
+
+#Prints Directional message on Terminal 
 info_msg= """
 Reading from the keyboard  and Publishing to Twist!
 ---------------------------------------------------
@@ -21,8 +23,9 @@ Up / Down : increase/decrease max speeds
 CTRL + C to quit
 """
 
+#This is Teleop function, sends the Linear and angular values to Twist msg
 def Teleop():
-    #This is Teleop function, sends the Linear and angular values to Twist msg
+    
     try:
         print(info_msg)
         while not rospy.is_shutdown():
@@ -50,13 +53,22 @@ def Teleop():
                 rospy.loginfo("Backward")
                 turtle_move(-var_linear_speed,0.0)
 
+            if key == 67:
+                rospy.loginfo("Right")
+                turtle_move(0.0,-var_angular_speed)
+
+            if key == 68:
+                rospy.loginfo("Left")
+                turtle_move(0.0,var_angular_speed)
+
             
 
     except Exception as e: # could not publish key values to cmd_vel
         print(e)
 
+
+#Adds Linear and angular values to Twist message as Linear.x and Angular.z
 def turtle_move(linear,angular):
-    #Adds Linear and angular values to Twist message as Linear.x and Angular.z
     try:
         twist = Twist()
         twist.linear.x = linear
@@ -71,17 +83,24 @@ def turtle_move(linear,angular):
     except Exception as e: #Error In Publishing Twist Message. 
         print(e)
 
-    
+
+#Main Function to this Program, which calls Teleop() and 
 if __name__ == '__main__':
     try:
         pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
         rospy.init_node('Turtlesim_teleoperator', anonymous=False)
         rospy.loginfo("Started publishing values")
         rate = rospy.Rate(100) # 10hz
+
+        #Linear and angular speed is set to 1.0 and 0.5 respectively
         linear_speed = 1.0
         angular_speed = 0.5
+
+        #Variable Linear and agular speed is Calculated. 
         var_linear_speed = linear_speed * 3.0
         var_angular_speed = angular_speed * 4.0
+
+        #Calls Teleop() Function
         Teleop()
         
     except rospy.ROSInterruptException: 
