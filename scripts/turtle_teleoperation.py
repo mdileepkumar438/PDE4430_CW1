187c86874b2b8ee2f80bb2082d1abf46b467194d
Dileep <dileep@gmail.com>
2022-11-13 11:51:10 +0400

    Teleoperation is changed also variable speed added

---
+++ scripts/turtle_teleoperation.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import getch
+import rospy
+from geometry_msgs.msg import Twist
+
+info_msg= """
+Reading from the keyboard  and Publishing to Twist!
+---------------------------------------------------
+Moving around:
+    w
+a   s   d
+
+---------------------------------------------------
+
+Up / Down : increase/decrease max speeds
+< / > : increase/decrease max angular speed
+
+---------------------------------------------------
+
+CTRL + C to quit
+"""
+
+def Teleop():
+    #This is Teleop function, sends the Linear and angular values to Twist msg
+    try:
+        print(info_msg)
+        while not rospy.is_shutdown():
+            key = ord(getch.getch())
+            print(key)
+            if key == 119:
+                rospy.loginfo("Forward")
+                turtle_move(linear_speed,0.0)
+            if key == 115:
+                rospy.loginfo("Backward")
+                turtle_move(-linear_speed,0.0)
+            if key == 100:
+                rospy.loginfo("Right")
+                turtle_move(0.0,-angular_speed)
+            if key == 97:
+                rospy.loginfo("Left")
+                turtle_move(0.0,angular_speed)
+
+            # Variable speed for keys Up / Down / left / Right arrows
+            if key == 65:
+                rospy.loginfo("Forward")
+                turtle_move(var_linear_speed,0.0)
+
+            if key == 66:
+                rospy.loginfo("Backward")
+                turtle_move(-var_linear_speed,0.0)
+
+            
+
+    except Exception as e: # could not publish key values to cmd_vel
+        print(e)
+
+def turtle_move(linear,angular):
+    #Adds Linear and angular values to Twist message as Linear.x and Angular.z
+    try:
+        twist = Twist()
+        twist.linear.x = linear
+        twist.linear.y = 0
+        twist.linear.z =0
+        twist.angular.x = 0 
+        twist.angular.y = 0
+        twist.angular.z = angular
+        pub.publish(twist)
+        rate.sleep()
+
+    except Exception as e: #Error In Publishing Twist Message. 
+        print(e)
+
+    
+if __name__ == '__main__':
+    try:
+        pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
+        rospy.init_node('Turtlesim_teleoperator', anonymous=False)
+        rospy.loginfo("Started publishing values")
+        rate = rospy.Rate(100) # 10hz
+        linear_speed = 1.0
+        angular_speed = 0.5
+        var_linear_speed = linear_speed * 3.0
+        var_angular_speed = angular_speed * 4.0
+        Teleop()
+        
+    except rospy.ROSInterruptException: 
+        pass
