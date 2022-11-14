356089d992be2de8efb0cce107778347a0e7e894
Dileep <dileep@gmail.com>
2022-11-14 15:27:24 +0400

    Initialized positions in main function and added few function (updatepos, distancecal, setvelocity.)

--- scripts/Auto_navigation.py
+++ scripts/Auto_navigation.py
@@ -3,36 +3,80 @@
 import rospy
 from geometry_msgs.msg import Twist 
 from geometry_msgs.msg import Pose2D 
-from turtlesim import Pose
+from turtlesim import Pose as pose
 
-#==============================================
+#================================================================
 #Calculates distance from Goal 
 # (Current x to destination x coordinat)
 def distancetogoal():
-    return Des_position.x - Current_position.x
-#==============================================
+    try:
+        return Des_position.x - Current_position.x
+
+    except Exception as e: # Error in Getting the distance to Goal
+        print(e)
 
 
+#================================================================
+#sets the velocity value, only if not reached the destination
+#else stop the Turtle
+def set_velocity():
 
+    try:
+        if (abs(distancetogoal())> Dis_tolerance):
+            vel_command.linear.x = linear_vel * distancetogoal()
+
+        else:
+            rospy.loginfo("Goal has been reached")
+            vel_command.linear.x=0
+    
+    except Exception as e1 : #Error in Settin up the velocity
+        print(e1)
+
+
+#================================================================
+#Update the current position and oreintation of Turtle
+def updatepose(data):  
+    try:
+        pose = data
+        pose.x = round(pose.x, 4)
+        pose.y = round(pose.y, 4)
+        pose.theta = round (pose.theta, 2)
+
+    except Exception as e2: #Error in Updating Pose
+        print(e2)
+
+
+#=================================================================
 
 
 
 #Main Function to this Program, which calls Teleop() and 
 if __name__ == '__main__':
     try:
+        # Publisher which will publish to the topic '/turtle1/cmd_vel'
         pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-        rospy.init_node('Turtlesim_teleoperator', anonymous=False)
+
+        pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose(), updatepose())
+
+        #Creates a node with name 'Turtlesim_Navigation',
+        # unique node (using anonymous=True).
+        rospy.init_node('Turtlesim_Navigation', anonymous=True)
+
         rospy.loginfo("Started publishing values")
-        rate = rospy.Rate(100) # 10hz
+        rate = rospy.Rate(10) # 10hz
         vel_command = Twist()
         Current_position = Pose2D()
         Des_position = Pose2D()
-        Goal = 1.3
-        linear_vel = 1.0
+        
+        linear_vel = 0.5
+        angular_vel = 0.5
         Dis_tolerance = 0.1
-
+        angular_tolerance = 0.1
+        gotodestination = False
+        
         #Initialized Variable
-        Des_position.x =  Goal
+        Des_position.x =  5.54
+        Des_position.y = 5.54
 
         #Initialized Twist msg
         vel_command.linear.x = 0.0
