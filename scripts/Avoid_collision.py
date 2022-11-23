7c4bacf460c28feb372862c3eccf92cc698979b0
Dileep <dileep@gmail.com>
2022-11-23 23:32:45 +0400

    added walls (borders to the window ) and also added avoid function which turns the turtle towards centre

--- scripts/Avoid_collision.py
+++ scripts/Avoid_collision.py
@@ -3,44 +3,85 @@
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
+from math import atan2
+msg = """
+Borders of the wall to avoid :
+
+x = [0.75, 10.5]
+y = [0.75, 10.5]
+"""
 
 
-
-   
-def avoid_collision(linear,angular):
-    cmd_value=Twist()
-    cmd_value.linear.x = linear
-    cmd_value.angular.z = angular 
-    pub.publish(cmd_value)
-    rate.sleep()
-     
-    
-#Update the destination point when a messages is published 
-def pose_update(data):  
-    try:
-
-        pose = data
-        pose.x = round(pose.x,2)
-        pose.y = round(pose.y,2)
-        #rospy.loginfo("hit on wall 1")
+class Avoid:
+    def __init__(self):
+        rospy.init_node('Turtlesim_Avoid_collision', anonymous=True)
+        # Publisher which will publish to '/turtle1/cmd_vel'
+        self.pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+        self.pose_subs = rospy.Subscriber('/turtle1/pose',Pose, self.pose_update)
+        self.rate = rospy.Rate(100)
         
-        if ((pose.x == 0.5) or (pose.y == 10.5)):
-            rospy.loginfo("hit")
-            avoid_collision(0.0,1.0)
-    except Exception as e2: #Error in Updating Pose
-        print(e2)
+        rospy.loginfo(msg)
+    
+
+    #Update the destination point when a messages is published 
+    def pose_update(self,data):  
+        try:
+            
+            self.pose = data
+            self.pose.x = round(self.pose.x,2)
+            self.pose.y = round(self.pose.y,2)
+            
+            cmd_value=Twist()
+
+            #Function for avoiding the wall and turning the turtle towards center
+            def avoid_wall():
+                
+
+                deltaX = 5.5 - self.pose.x
+                deltaY = 5.5 - self.pose.y
+                #Returns the values in radians
+                Des_Heading = atan2(deltaY, deltaX)
+                angle_to_rotate = Des_Heading - self.pose.theta
+                cmd_value.linear.x = 0.2
+                cmd_value.angular.z = 4* angle_to_rotate
+
+                #Publishes the Twist values to cmd_vel 
+                self.pub.publish(cmd_value)
+                self.rate.sleep()
+        
+            #Checks the turtle is touching the wall
+            if (self.pose.x <= x[0] ):
+                
+                rospy.loginfo("I hit the left wall")
+                avoid_wall()
+
+
+            if (self.pose.y <= y[0]):
+                rospy.loginfo("I hit the bottom wall")
+                avoid_wall()
+
+
+            if (self.pose.x >=x[1]):
+                rospy.loginfo("I hit the Right wall")
+                avoid_wall()
+
+            if (self.pose.y >=y[1] ) :
+
+                rospy.loginfo("I hit the Top wall")
+                avoid_wall()
+
+        except Exception as e2: #Error in Updating Pose
+            print(e2)
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('Turtlesim_Avoid_collision', anonymous=True)
-        # Publisher which will publish to '/turtle1/cmd_vel'
-        pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-        pose_subs = rospy.Subscriber('/turtle1/pose',Pose, pose_update)
-        rate = rospy.Rate(10)
-        #avoid_collision()
-        #Testing the function
-        #avoid_collision()
         
+        x = [0.75, 10.5]
+        y = [0.75, 10.5]
+
+        
+        Avoid()
+        #avoid.avoid_collision()
 
         rospy.spin()
     except rospy.ROSInterruptException: 
